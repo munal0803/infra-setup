c7f7fc2bd1cf8c0137d497378d66c4a2352d8f7f
Munal Jain <75110825+munal0803@users.noreply.github.com>
2026-04-05 14:00:51 +0530

    initial infra

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  credentials = var.gcp_credentials
+  project     = var.project_id
+  region      = var.region
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+    default = "booming-oarlock-457013-e0"
+}
+variable "region" {
+  default = "us-central1"
+}
+variable "gcp_credentials" {
+  sensitive = true
+}
